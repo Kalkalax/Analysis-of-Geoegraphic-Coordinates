a808cc7553332ed7b80e01773438b2fc4ed3c269
Kamil Kalarus <kalkalax@gmail.com>
2024-08-13 12:00:18 +0200

    Adding the ConfigurationParameters class

---
+++ src/configurationparameters.py
@@ -0,0 +1,7 @@
+class ConfigurationParameters:
+    host = "localhost"
+    port = "5432"
+    dbname = "Analysis-of-Geoegraphic-Coordinates"
+    tbname = "Analysis-of-Geoegraphic-Coordinates-Data"
+    user = "postgres"
+    password = "123456789"
---
+++ tests/test_filereader.py
@@ -0,0 +1,28 @@
+import pytest
+
+from src.filereader import FileReader
+
+class TestReadFile:
+
+    def test_readFile_ok(self):
+
+        pass
+
+    def test_readFile_FileNotFoundError(self):
+
+        pass
+
+    def test_readFile_ParserError(self):
+
+        pass
+
+    def test_readFile_Exception(self):
+
+        pass
+
+class TestCheckPointName:
+
+    def test_readFile2(self):
+
+        pass
+    
